836aa2a4bf90a33bcc35fb484300c811495075b0
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-08-09 17:32:47 +0300

    Update CI

--- .github/workflows/test-gcc.sh
+++ .github/workflows/test-gcc.sh
@@ -12,11 +12,15 @@ startTest "GCC compilation"
 BUILDDIR="build/gh-checks/gcc/"
 mkdir -p "${CURDIR}/../../${BUILDDIR}"
 
+# Remove any cached version of conan profiles
+rm -fR ${CURDIR}/../../build/gh-checks/conan-cache/profiles
+
 # Run docker with action-cxx-toolkit to check our code
 docker run ${DOCKER_RUN_PARAMS} \
+    -e INPUT_CHECKS='build test install warnings' \
+    -e INPUT_PREBUILD_COMMAND='conan profile detect && conan create /github/workspace/external/context-core-api --build=missing -s compiler.cppstd=17 ; rm -fR ~/.conan2/profiles' \
     -e INPUT_BUILDDIR="/github/workspace/${BUILDDIR}" \
     -e INPUT_CC='gcc' \
-    -e INPUT_CHECKS='build test install warnings' \
     -e INPUT_CONANFLAGS="--output-folder /github/workspace/${BUILDDIR}" \
     -e INPUT_CMAKEFLAGS="-DCMAKE_TOOLCHAIN_FILE=/github/workspace/${BUILDDIR}/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Release" \
     -e INPUT_CTESTFLAGS="--test-dir /github/workspace/${BUILDDIR}/test/" \
