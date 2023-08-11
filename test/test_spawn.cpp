54c0dbec802ad7c55c31c71bccbb28b9a0cd5db6
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-08-11 14:33:50 +0300

    Add simple spawn test

---
+++ test/test_spawn.cpp
@@ -0,0 +1,30 @@
+#include "async_oper_state.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <semaphore>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+TEST_CASE("spawn can execute work", "[spawn]") {
+  // Arrange
+  bool called{false};
+  std::binary_semaphore done{0};
+
+  // Act
+  concore2full::async_oper_state<int> op;
+  op.spawn([&]() -> int {
+    called = true;
+    done.release();
+    return 13;
+  });
+  done.acquire();
+  std::this_thread::sleep_for(1ms);
+  concore2full::global_thread_pool().clear();
+  auto res = op.await();
+
+  // Assert
+  REQUIRE(called);
+  REQUIRE(res == 13);
+}
