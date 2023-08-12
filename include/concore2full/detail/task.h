c92711f664c9927cabf442e39854b9909064af09
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-08-12 16:54:32 +0300

    Start adding tasks as interface for thread pool.
    This doesn't fully work, as we don't have a place yet to store the task objects.

---
+++ include/concore2full/detail/task.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <concepts>
+#include <functional>
+
+namespace concore2full::detail {
+
+/// @brief Base class for task types.
+///
+/// This interface allows one to easily queue tasks to be executed. It provides a way to execute the
+/// task, and also defines an intrusive list structure to able to chain tasks.
+struct task_base {
+  task_base* next_;
+  /// Execute the task.
+  virtual void execute() noexcept = 0;
+};
+
+/// @brief Concrete task type that just calls a user-provided function.
+/// @tparam Fn The type of the function wrapped by this task.
+template <std::invocable Fn> struct fun_task : task_base {
+  Fn f_;
+  wrapper_task(Fn&& f) : f_(std::forward<Fn>(f)) {}
+
+  void execute() noexcept override { std::invoke(f_); }
+}
+
+} // namespace concore2full::detail
