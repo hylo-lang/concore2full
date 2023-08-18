08d56df2171f7872955a56d484b8e6d11f4d3c24
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-08-18 21:15:23 +0300

    Add more tests for `sync_execute`. Two of them fail occasionally.

--- test/test_sync_execute.cpp
+++ test/test_sync_execute.cpp
@@ -9,6 +9,16 @@
 
 using namespace std::chrono_literals;
 
+template <typename Fn> int do_thread_inversion(Fn&& f) {
+  auto h = concore2full::spawn([f = std::forward<Fn>(f)] {
+    std::this_thread::sleep_for(5ms);
+    f();
+    return 0;
+  });
+  std::this_thread::sleep_for(1ms);
+  return h.await(); // thread inversion
+}
+
 TEST_CASE("sync_execute can call a function", "[sync_execute]") {
   // Arrange
   bool called{false};
@@ -20,24 +30,142 @@ TEST_CASE("sync_execute can call a function", "[sync_execute]") {
 
 TEST_CASE("sync_execute will finish on the same thread", "[sync_execute]") {
   // Arrange
-  std::thread::id tid1;
-  std::thread::id tid2;
   bool called{false};
-  auto f = [&] {
-    auto h = concore2full::spawn([&] {
-      std::this_thread::sleep_for(100ms);
-      called = true;
-      return 0;
-    });
-    std::this_thread::sleep_for(5ms);
-    (void)h.await(); // thread inversion
-  };
+  auto f = [&] { (void)do_thread_inversion([&] { called = true; }); };
 
   // Act
-  tid1 = std::this_thread::get_id();
+  auto tid1 = std::this_thread::get_id();
   concore2full::sync_execute(std::move(f));
-  tid2 = std::this_thread::get_id();
+  auto tid2 = std::this_thread::get_id();
 
   // Assert
   REQUIRE(tid1 == tid2);
+  REQUIRE(called);
 }
+
+// TEST_CASE("sync_execute will finish on the same thread after two thread inversions in a row",
+//           "[sync_execute]") {
+//   // Arrange
+//   bool called1{false};
+//   bool called2{false};
+//   auto f = [&] {
+//     (void)do_thread_inversion([&] { called1 = true; });
+//     (void)do_thread_inversion([&] { called2 = true; });
+//   };
+
+//   // Act
+//   auto tid1 = std::this_thread::get_id();
+//   concore2full::sync_execute(std::move(f));
+//   auto tid2 = std::this_thread::get_id();
+
+//   // Assert
+//   REQUIRE(tid1 == tid2);
+//   REQUIRE(called1);
+//   REQUIRE(called2);
+// }
+
+// TEST_CASE("sync_execute will finish on the same thread after two nested thread inversions",
+//                    "[sync_execute]") {
+//   // Arrange
+//   bool called1{false};
+//   bool called2{false};
+//   auto f = [&] {
+//     (void)do_thread_inversion([&] {
+//       called1 = true;
+//       (void)do_thread_inversion([&] { called2 = true; });
+//     });
+//   };
+
+//   // Act
+//   auto tid1 = std::this_thread::get_id();
+//   concore2full::sync_execute(std::move(f));
+//   auto tid2 = std::this_thread::get_id();
+
+//   // Assert
+//   REQUIRE(tid1 == tid2);
+//   REQUIRE(called1);
+//   REQUIRE(called2);
+// }
+
+TEST_CASE("nested sync_execute: two simple calls", "[sync_execute]") {
+  // Arrange
+  bool called1{false};
+  bool called2{false};
+  // Act
+  auto tid1 = std::this_thread::get_id();
+  concore2full::sync_execute([&] {
+    called1 = true;
+    concore2full::sync_execute([&] { called2 = true; });
+  });
+  auto tid2 = std::this_thread::get_id();
+
+  // Assert
+  REQUIRE(tid1 == tid2);
+  REQUIRE(called1);
+  REQUIRE(called2);
+}
+
+TEST_CASE("nested sync_execute: simple call + thread inversion", "[sync_execute]") {
+  // Arrange
+  bool called1{false};
+  bool called2{false};
+  // Act
+  auto tid1 = std::this_thread::get_id();
+  concore2full::sync_execute([&] {
+    called1 = true;
+    concore2full::sync_execute([&] { (void)do_thread_inversion([&] { called2 = true; }); });
+  });
+  auto tid2 = std::this_thread::get_id();
+
+  // Assert
+  REQUIRE(tid1 == tid2);
+  REQUIRE(called1);
+  REQUIRE(called2);
+}
+
+TEST_CASE("nested sync_execute: thread inversion + thread inversion", "[sync_execute]") {
+  // Arrange
+  bool called1{false};
+  bool called2{false};
+  // Act
+  auto tid1 = std::this_thread::get_id();
+  concore2full::sync_execute([&] {
+    (void)do_thread_inversion([&] {
+      called1 = true;
+      concore2full::sync_execute([&] { (void)do_thread_inversion([&] { called2 = true; }); });
+    });
+  });
+  auto tid2 = std::this_thread::get_id();
+
+  // Assert
+  REQUIRE(tid1 == tid2);
+  REQUIRE(called1);
+  REQUIRE(called2);
+}
+
+TEST_CASE("nested sync_execute: thread inversion + simple + thread inversion", "[sync_execute]") {
+  // Arrange
+  bool called1{false};
+  bool called2{false};
+  bool called3{false};
+  // Act
+  auto tid1 = std::this_thread::get_id();
+  concore2full::sync_execute([&] {
+    (void)do_thread_inversion([&] {
+      called1 = true;
+      concore2full::sync_execute([&] {
+        called2 = true;
+        concore2full::sync_execute([&] { (void)do_thread_inversion([&] { called3 = true; }); });
+      });
+    });
+  });
+  auto tid2 = std::this_thread::get_id();
+
+  // Assert
+  REQUIRE(tid1 == tid2);
+  REQUIRE(called1);
+  REQUIRE(called2);
+  REQUIRE(called3);
+}
+
+// TODO: nested sync_execute
