3fb8037ec4674d406f6eac21ed830c8203f185ec
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-09-11 22:16:17 +0300

    Improve spawn documentation.

--- include/concore2full/spawn.h
+++ include/concore2full/spawn.h
@@ -8,33 +8,34 @@
 
 namespace concore2full {
 
-/// @brief The state of a spawned execution.
-///
-/// @tparam Fn The type of the functor used for spawning concurrent work.
-///
-/// This represents the result of a `spawn` call, containing the state associated with the spawned
-/// work. It will hold the data needed to execute the spawned computation and get its results.
-///
-/// It can only be constructed through calling the `spawn()` function.
-///
-/// It provides mechanisms for the main thread of execution to *await* the finish of the
-/// computation. That is, continue when the computation is finished. If the main thread of execution
-/// finishes first, then *thread inversion* will happen on the await point: the spawned thread will
-/// continue the main work, while the main thread will be suspended. However, no OS thread will be
-/// blocked.
-///
-/// It exposes the type of the result of the spanwed computation.
-///
-/// If the object is destryoed before the spawned work finishes, the work will be cancelled.
-///
-/// @sa spawn()
+/**
+ * @brief The state of a spawned execution.
+ * @tparam Fn The type of the functor used for spawning concurrent work.
+ *
+ * This represents the result of a `spawn` call, containing the state associated with the spawned
+ * work. It will hold the data needed to execute the spawned computation and get its results.
+ *
+ * It can only be constructed through calling the `spawn()` function.
+ *
+ * It provides mechanisms for the main thread of execution to *await* the finish of the
+ * computation. That is, continue when the computation is finished. If the main thread of execution
+ * finishes first, then *thread inversion* will happen on the await point: the spawned thread will
+ * continue the main work, while the main thread will be suspended. However, no OS thread will be
+ * blocked.
+ *
+ * It exposes the type of the result of the spanwed computation.
+ *
+ * If the object is destryoed before the spawned work finishes, the work will be cancelled.
+ *
+ * @sa spawn()
+ */
 template <typename Fn> class spawn_state : private detail::task_base {
 public:
   ~spawn_state() = default;
-  // No copy, no move
+  //! No copy, no move
   spawn_state(const spawn_state&) = delete;
 
-  /// The type returned by the spawned computation.
+  //! The type returned by the spawned computation.
   using res_t = std::invoke_result_t<Fn>;
 
   /// @brief Await the result of the computation
@@ -45,12 +46,23 @@ public:
   /// return the result of the work. If the main thread of exection arrives here before the work is
   /// completed, a "thread inversion" happens, and the main thread of execution continues work on
   /// the coroutine that was just spawned.
+
+  /**
+   * @brief Await the result of the computation.
+   * @return The result of the computation; throws if the operation was cancelled.
+   *
+   * If the main thread of execution arrives at this point after the work is done, this will simply
+   * return the result of the work. If the main thread of exection arrives here before the work is
+   * completed, a "thread inversion" happens, and the main thread of execution continues work on
+   * the coroutine that was just spawned.
+   */
   res_t await() {
     on_main_complete();
     return res_;
   }
 
 private:
+  //! Desribes current state of the execution.
   enum class sync_state {
     both_working,
     main_finishing,
@@ -58,11 +70,11 @@ private:
     async_finished,
   };
 
-  /// The function that needs to be executed in the spawned work.
+  //! The function that needs to be executed in the spawned work.
   Fn f_;
-  /// The location where we need to store the result
+  //! The location where we need to store the result.
   res_t res_;
-  /// The state of the computation, with respect to reaching the await point.
+  //! The state of the computation, with respect to reaching the await point.
   std::atomic<sync_state> sync_state_{sync_state::both_working};
   //! Indicates that the async processing has started (continuation is set).
   std::atomic<bool> async_started_{false};
@@ -73,6 +85,14 @@ private:
 
   /// @brief  Called when the async work is completed
   /// @return The continuation that the work coroutine needs to do next (if there is any).
+
+  /**
+   * @brief Called when the async work is completed
+   * @param c The continuation for the thread used to execute the async work.
+   * @return The continuation that the work coroutine needs to do next (if there is any).
+   *
+   * @sa on_main_complete()
+   */
   detail::continuation_t on_async_complete(detail::continuation_t c) {
     sync_state expected{sync_state::both_working};
     if (sync_state_.compare_exchange_strong(expected, sync_state::async_finished)) {
@@ -92,7 +112,15 @@ private:
     }
   }
 
-  /// Called when the main thread of execution completes.
+  /**
+   * @brief Called when the main thread of execution completes.
+   *
+   * If this finishes before the async work, then a thread switch will happen. The thread that
+   * arrives here continues to execute on the control flow of the thread that started the async
+   * work.
+   *
+   * @sa on_async_complete()
+   */
   void on_main_complete() {
     sync_state expected{sync_state::both_working};
     if (sync_state_.compare_exchange_strong(expected, sync_state::main_finishing)) {
@@ -114,6 +142,7 @@ private:
     // This point will be executed by the thread that finishes last.
   }
 
+  //! Executes the async work.
   void execute(int) noexcept {
     profiling::duplicate_zones_stack scoped_zones_stack{zones_};
     (void)detail::callcc([this](detail::continuation_t thread_cont) -> detail::continuation_t {
@@ -131,14 +160,21 @@ private:
 
   template <typename F> friend auto spawn(F&& f);
 
+  //! Private constructor. `spawn` will call this.
   spawn_state(Fn&& f) : f_(std::forward<Fn>(f)) { global_thread_pool().enqueue(this); }
 };
 
-/// @brief  Spawn work with the default scheduler
-/// @tparam Fn The type of the function to execute
-/// @param f The function representing the work that needs to be executed concurrently.
-///
-/// This will use the default scheduler to spawn new work concurrently.
+/**
+ * @brief Spawn work with the default scheduler.
+ * @tparam Fn The type of the function to execute.
+ * @param f The function representing the work that needs to be executed asyncrhonously.
+ * @return A `spawn_state` object; this object cannot be copied or moved
+ *
+ * This will use the default scheduler to spawn new work concurrently.
+ *
+ * The returned state object needs to stay alive for the entire duration of the computation. If the
+ * object is destructed, the computation is cancelled.
+ */
 template <typename Fn> inline auto spawn(Fn&& f) {
   using op_t = spawn_state<Fn>;
   return op_t{std::forward<Fn>(f)};
