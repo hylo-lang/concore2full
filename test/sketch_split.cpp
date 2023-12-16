74c68b9a20dfe181fd6afabed9cc42cdf93da792
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-12-16 21:07:36 +0200

    Sketch of `split` using weakly-structure concurrency.

--- test/sketch_split.cpp
+++ test/sketch_split.cpp
@@ -21,7 +21,7 @@ TEST_CASE("sketch of implementing a P2300 `split` -- structured concurrency", "[
 
   // Start applying gaussian blur to the document.
   auto future1 = concore2full::spawn([=] { return apply_gaussian_blur(initial_doc); });
-  // maybe do something elese in the meantime
+  // maybe do something else in the meantime
   document doc1 = future1.await();
   // Save this document, and start applying sharpen to the new document.
   auto future2 = concore2full::spawn([=] { save(doc1); });
@@ -31,4 +31,27 @@ TEST_CASE("sketch of implementing a P2300 `split` -- structured concurrency", "[
 
   // Save the final document.
   save(doc2);
+}
+
+document create_doc() { return document{"initial_state"}; }
+using future_create_doc_t = decltype(concore2full::escaping_spawn(create_doc));
+
+auto split_save_sharpen(future_create_doc_t future1) {
+  // Do something only after the future is ready.
+  document doc1 = future1.await();
+  // Save this document, and start applying sharpen to the new document.
+  auto future2 = concore2full::spawn([=] { save(doc1); });
+  document doc2 = apply_sharpen(doc1);
+  // Ensure that saving is done.
+  future2.await();
+  return doc2;
+}
+
+TEST_CASE("sketch of implementing a P2300 `split` -- weekly-structured concurrency", "[examples]") {
+
+  // Get a future that creates a document.
+  auto future1 = concore2full::escaping_spawn(create_doc);
+  // Save the document resulting from `future1` and apply sharpen.
+  auto doc = split_save_sharpen(future1);
+  printf("resulting document: %s\n", doc.text.c_str());
 }
