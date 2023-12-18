f4f673f1449c5408e900c64d6405613d0547e5f2
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-12-18 21:31:17 +0200

    Fix clang-format issue,

--- test/test_thread_pool.cpp
+++ test/test_thread_pool.cpp
@@ -2,9 +2,9 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-#include <functional>
 #include <atomic>
 #include <chrono>
+#include <functional>
 #include <latch>
 
 using namespace std::chrono_literals;
