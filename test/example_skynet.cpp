0c9d185f743803c2d0b44b1ac07eead5acb74cba
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2024-03-14 21:58:44 +0200

    Weekly-structured concurrency skynet example.

--- test/example_skynet.cpp
+++ test/example_skynet.cpp
@@ -10,7 +10,7 @@
 
 using namespace std::chrono_literals;
 
-uint64_t skynet(int num, int size, int div) {
+uint64_t skynet_strict(int num, int size, int div) {
   concore2full::profiling::zone zone{CURRENT_LOCATION()};
   zone.set_param("num", int64_t(num));
   zone.set_param("size", int64_t(size));
@@ -20,25 +20,25 @@ uint64_t skynet(int num, int size, int div) {
   } else {
     const int sub_size = size / div;
     int sub_num = num;
-    auto f0 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f0 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
     sub_num += sub_size;
-    auto f1 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f1 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
     sub_num += sub_size;
-    auto f2 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f2 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
     sub_num += sub_size;
-    auto f3 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f3 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
     sub_num += sub_size;
-    auto f4 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f4 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
     sub_num += sub_size;
-    auto f5 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f5 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
     sub_num += sub_size;
-    auto f6 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f6 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
     sub_num += sub_size;
-    auto f7 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f7 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
     sub_num += sub_size;
-    auto f8 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f8 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
     sub_num += sub_size;
-    auto f9 = concore2full::spawn([=] { return skynet(sub_num, sub_size, div); });
+    auto f9 = concore2full::spawn([=] { return skynet_strict(sub_num, sub_size, div); });
 
     uint64_t sum = 0;
     sum += f0.await();
@@ -56,10 +56,42 @@ uint64_t skynet(int num, int size, int div) {
   }
 }
 
-uint64_t run_benchmark() {
+uint64_t skynet_weak(int num, int size, int div);
+
+struct skynet_weak_fun {
+  int num_;
+  int size_;
+  int div_;
+
+  uint64_t operator()() const { return skynet_weak(num_, size_, div_); }
+};
+
+uint64_t skynet_weak(int num, int size, int div) {
   concore2full::profiling::zone zone{CURRENT_LOCATION()};
-  // return skynet(0, 1000000, 10);
-  return skynet(0, 1000, 10);
+  zone.set_param("num", int64_t(num));
+  zone.set_param("size", int64_t(size));
+  if (size == 1) {
+    concore2full::profiling::zone z1{CURRENT_LOCATION_N("skynet-1")};
+    return uint64_t(num);
+  } else {
+    using future_t = decltype(concore2full::escaping_spawn(skynet_weak_fun{0, 0, 0}));
+
+    // Spawn the sub-tasks.
+    const int sub_size = size / div;
+    std::vector<future_t> futures;
+    for (int i = 0; i < div; i++) {
+      int sub_num = num + i * sub_size;
+      futures.push_back(concore2full::escaping_spawn(skynet_weak_fun{sub_num, sub_size, div}));
+    }
+
+    // Wait for the results.
+    uint64_t sum = 0;
+    for (int i = 0; i < div; i++) {
+      sum += futures[i].await();
+    }
+
+    return sum;
+  }
 }
 
 TEST_CASE("skynet microbenchmark example", "[benchmark]") {
@@ -67,10 +99,25 @@ TEST_CASE("skynet microbenchmark example", "[benchmark]") {
   concore2full::profiling::zone zone{CURRENT_LOCATION()};
 
   auto now = std::chrono::high_resolution_clock::now();
-  uint64_t result = run_benchmark();
+  // uint64_t result = skynet_strict(0, 1'000'000, 10);
+  uint64_t result = skynet_strict(0, 100'000, 10);
   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
       std::chrono::high_resolution_clock::now() - now);
 
   printf("Result: %" PRIu64 " in %d ms\n", result, int(duration.count()));
-  REQUIRE(result == 499500);
+  REQUIRE(result == 4999950000);
+}
+
+TEST_CASE("skynet microbenchmark example (weakly structured concurrency)", "[benchmark]") {
+  concore2full::profiling::emit_thread_name_and_stack("main");
+  concore2full::profiling::zone zone{CURRENT_LOCATION()};
+
+  auto now = std::chrono::high_resolution_clock::now();
+  // uint64_t result = skynet_weak(0, 1'000'000, 10);
+  uint64_t result = skynet_weak(0, 100'000, 10);
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::high_resolution_clock::now() - now);
+
+  printf("Result: %" PRIu64 " in %d ms\n", result, int(duration.count()));
+  REQUIRE(result == 4999950000);
 }
