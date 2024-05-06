cb4dbc873028841b635f89079070723181a88b05
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2024-05-06 19:30:21 +0300

    Drop conan support. The build is based on cmake.

--- .github/workflows/test-clang.sh
+++ .github/workflows/test-clang.sh
@@ -18,12 +18,10 @@ rm -fR ${CURDIR}/../../build/gh-checks/conan-cache/profiles
 # Run docker with action-cxx-toolkit to check our code
 docker run ${DOCKER_RUN_PARAMS} \
     -e INPUT_CHECKS='build test install warnings' \
-    -e INPUT_PREBUILD_COMMAND='conan profile detect && conan create /github/workspace/external/context-core-api --build=missing -s compiler.cppstd=17 ; rm -fR ~/.conan2/profiles' \
     -e INPUT_BUILDDIR="/github/workspace/${BUILDDIR}" \
     -e INPUT_CC='clang-13' \
-    -e INPUT_CONANFLAGS="--output-folder /github/workspace/${BUILDDIR}" \
-    -e INPUT_CMAKEFLAGS="-DCMAKE_TOOLCHAIN_FILE=/github/workspace/${BUILDDIR}/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Release" \
-    -e INPUT_CTESTFLAGS="--test-dir /github/workspace/${BUILDDIR}/test/" \
+    -e INPUT_CMAKEFLAGS="-DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=On" \
+    -e INPUT_CTESTFLAGS="--test-dir /github/workspace/${BUILDDIR}/test" \
     $IMAGENAME
 status=$?
 printStatus $status
